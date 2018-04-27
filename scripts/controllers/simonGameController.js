366fbf1bc67ca1eecf00e5ecfeea2393770f76ef
Neil Mosunic <neilm813@gmail.com>
2018-04-26 22:11:04 -0700

    record chosen color sequence

--- scripts/controllers/simonGameController.js
+++ scripts/controllers/simonGameController.js
@@ -1,4 +1,24 @@
 angular.module("simonGame").controller("simonGameController", function ($scope) {
 
-  $scope.test = 'expression';
+    $scope.coloredSquares = {
+      red: {
+        isLowOpacity: true
+      },
+      blue: {
+        isLowOpacity: true
+      },
+      green: {
+        isLowOpacity: true
+      },
+      yellow: {
+        isLowOpacity: true
+      }
+    };
+
+    var chosenSequence = [];
+
+    $scope.squareClick = function(color) {
+      chosenSequence.push(color);
+      console.log(chosenSequence.toString());
+    }
 });
