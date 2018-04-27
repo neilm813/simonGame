b706058a681e024b0d2c8987c7bce55cd369973d
Neil Mosunic <neilm813@gmail.com>
2018-04-26 23:57:56 -0700

    generate random color sequence

--- scripts/controllers/simonGameController.js
+++ scripts/controllers/simonGameController.js
@@ -16,6 +16,19 @@ angular.module("simonGame").controller("simonGameController", function ($scope, 
     };
 
     var chosenSequence = [];
+    var simonSequence = [];
+    var possibleColors = Object.keys($scope.coloredSquares);
+    addColorToSimonSequence();
+
+    function randomIntFromInterval (min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function addColorToSimonSequence() {
+      var randomIdx = randomIntFromInterval(0, possibleColors.length - 1);
+      simonSequence.push(possibleColors[randomIdx]);
+      console.log(simonSequence);
+    }
 
     $scope.squareClick = function(color) {
       var clickedSquare = $scope.coloredSquares[color];
